b1954392f35acefb5c821ea8970a9020b9711e17
Silverflash-Xu <76473523+Silverflash-Xu@users.noreply.github.com>
2022-03-25 18:59:12 -0400

    Add files via upload

---
+++ search-photos-copy/search-photos.py
@@ -0,0 +1,83 @@
+import json
+import boto3
+import requests
+import logging
+
+logger = logging.getLogger()
+
+
+# logger.setLevel(logging.DEBUG)
+
+
+def lambda_handler(event, context):
+    # print("event",event)
+
+    # msg = event["multiValueQueryStringParameters"]['q']
+    query = event["queryStringParameters"]['q']
+    print("query", query)
+
+    # Define the client to interact with Lex
+    client = boto3.client('lex-runtime')
+    response = client.post_text(botName='SearchBot',
+                                botAlias='searchAlias',
+                                userId='testuser',
+                                inputText=query)
+    print("response", response)
+
+    labels = []
+    if 'slots' in response:
+        for slot in response['slots']:
+            labels.append(response['slots'][slot])
+
+    labels = [x for x in labels if x is not None]
+
+    query = {
+        "query": {
+            "bool": {
+                "should": []
+            }
+        }
+    }
+    for label in labels:
+        new_query = {
+            "term": {
+                "labels": label
+            }
+        }
+        query['query']['bool']['should'].append(new_query)
+
+    host = 'https://search-photos-kqcgjzgvw4w4n4r7kn6cvlhthu.us-east-1.es.amazonaws.com'  # the OpenSearch Service domain, including https://
+    index = 'photos'
+    type = '_search'
+    url = host + '/' + index + '/' + type
+    auth = ('admin', 'Admin..123')
+    headers = {"Content-Type": "application/json"}
+
+    es_res_raw = requests.get(url, auth=auth, headers=headers, data=json.dumps(query))
+    es_res = json.loads(es_res_raw.text)
+    
+    res_list = es_res['hits']['hits']
+
+    print('\nSearch results:')
+    print(es_res)
+    res_str = ''
+    
+    if len(res_list) != 0:
+        for item in res_list:
+            if item['_source']['objectKey'] not in res_str:
+                res_str += item['_source']['objectKey'] + ","
+    
+        res_str = res_str.rstrip(res_str[-1])
+    print(res_str)
+
+    
+    return {
+        'statusCode': 200,
+        'body': json.dumps(res_str),
+        'headers':{
+            'Access-Control-Allow-Origin': '*'
+        }
+    }
+
+    # postText input text is q,
+    # return dispatch(event)
